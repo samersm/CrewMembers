a37a7402b8748bfe0d5b1dd7d2d4a2ce9e53067f
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-07-11 18:48:11 +0300

    Connected MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the CrewMembers API...' }));
+
+// Connect Database
+connectDB();
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
