f5fdce1c89a8f3869395d5363f8dba451349ce51
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-07-19 19:50:52 +0300

    Finished api express app & Initialize the front end environment

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,67 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER,
+    SET_ALERT,
+    REMOVE_ALERT
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Due',
+                email: 'john@example.com',
+                phone: '555-555-5555',
+                type: 'professional'
+            },
+            {
+                id: 2,
+                name: 'Jack Smith',
+                email: 'jack@example.com',
+                phone: '333-333-3333',
+                type: 'professional'
+            },
+            {
+                id: 3,
+                name: 'Lili Lauren',
+                email: 'lili@example.com',
+                phone: '222-222-22222',
+                type: 'personal'
+            }
+        ]
+    };
+
+    const [state, dispatch] = useReducer(contactReducer, initialState);
+
+    // Add Contact
+
+    // Delete Contact
+
+    // Set Current
+
+    // Clear Current
+
+    // Update Contact
+
+    // Filter Contacts
+
+    // Clear Filter
+
+    return (
+        <ContactContext.Provider value={{ contacts: state.contacts }}>
+            {props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../models/User');
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
 
 // @route      POST api/user
 // @desc       Register a user
@@ -11,13 +14,33 @@ router.post('/', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 4 or more characters').isLength({ min: 4 })
 ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        res.send('Passed!');
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ msg: "User already exists" });
+                user = new User({ name, email, password });
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+                await user.save();
+                const payload = { user: { id: user.id } };
+                jwt.sign(payload, config.get(jwtSecret), { expiresIn: 3600000 }, (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
+            }
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     });
 
 module.exports = router;
